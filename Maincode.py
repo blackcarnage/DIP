c4976f5cd805e9d99a94760a3f424b1b56a7fb2a
blackcarnage <86094968+blackcarnage@users.noreply.github.com>
2022-12-13 18:24:00 +0530

    Update Maincode.py

--- Maincode.py
+++ Maincode.py
@@ -3,11 +3,11 @@ from PIL import Image
 
 import cv2 
 import matplotlib.pyplot as plt
-import os
+#import os
 import numpy as np
-from scipy.interpolate import griddata
+#from scipy.interpolate import griddata
 import scipy
-from google.colab.patches import cv2_imshow
+#from google.colab.patches import cv2_imshow
 
 
 
