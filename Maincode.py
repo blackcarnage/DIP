d91cc2e92467a8524095e8099685d659a0b8955b
blackcarnage <86094968+blackcarnage@users.noreply.github.com>
2022-12-13 19:19:13 +0530

    Update Maincode.py

--- Maincode.py
+++ Maincode.py
@@ -2,11 +2,11 @@ import streamlit as st
 from PIL import Image
 
 import cv2 
-#import matplotlib
-#import os
+import matplotlib
+import os
 import numpy
 #from scipy.interpolate import griddata
-#import scipy
+import scipy
 #from google.colab.patches import cv2_imshow
 
 
