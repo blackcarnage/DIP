bc2cdefa3cf1096a9c7d99612f49da9df8abf4ff
blackcarnage <86094968+blackcarnage@users.noreply.github.com>
2022-12-13 18:13:16 +0530

    Update Maincode.py

--- Maincode.py
+++ Maincode.py
@@ -1,6 +1,23 @@
 import streamlit as st
-import cv2
-st.title("Hello GeeksForGeeks !!!")
+from PIL import Image
+
+import cv2 
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+from scipy.interpolate import griddata
+import scipy
+from google.colab.patches import cv2_imshow
+
+
+
+
+st.title("Digital Image Processing")
+st.header("End Semester Project")
+
+img = Image.open("a.jpg");
+st.image(img);
+
 
 
 
